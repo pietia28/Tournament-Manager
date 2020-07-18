97a7e3da3fa8c755e20e462d7d40e0ed88c3018d
Piotr Głowacki <pege28@wp.pl>
2020-07-18 20:11:04 +0200

    very first config

---
+++ src/test/java/pl/pg/tmanager/TmanagerApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.pg.tmanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TmanagerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
